6cae9e0387588b3a16f43bc6c02bbecb899daa46
njorojoe11173 <njorojoe11173@gmail.com>
2024-12-12 17:10:51 +0000

    Utility for handling errors consistently across the application.

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,62 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+      this.isOperational = true;
+  
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+  };
+  
+  const handleDuplicateFieldsDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+  };
+  
+  const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+  };
+  
+  const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  };
+  
+  const sendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    } else {
+      // Programming or other unknown error: don't leak error details
+      console.error('ERROR 💥', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went very wrong!'
+      });
+    }
+  };
+  
+  export {
+    AppError,
+    handleCastErrorDB,
+    handleDuplicateFieldsDB,
+    handleValidationErrorDB,
+    sendErrorDev,
+    sendErrorProd
+  };
